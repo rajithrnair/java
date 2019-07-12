1220809efc7307d6af99e7833ca061eb74a861dc
rajith <rajithrnair@gmail.com>
2019-07-12 14:44:59 +0530

    hu

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, I am here in the terminal window.
-      System.out.println("Hello all, Welcome to Bangalore from UK, my first automatic CI");
+      System.out.println("Hello all, Rajat to Bangalore from UK, my first automatic CI");
    }
 }
